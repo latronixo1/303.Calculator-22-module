51a6d005a1b99262610dac648e14cbc7b339cd31
latronixo <pamerra@mail.ru>
2025-03-27 17:08:26 +0300

    307. Предотратил повторный ввод разделителя дробной части "."

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -41,52 +41,38 @@ class ViewController: UIViewController {
 
     //Что произойдет когда будет нажата цифра или точка на клавиатуре
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //если нам нужно начать новую строку
-//        if isFinishedTypingNumber {
-//            
-//        }   //if isFinishedTypingNumber {
 
-            //извлекаем текст из нажатой кнопки
-            if let numValue = sender.currentTitle {
-                //извлекаем текст из надписи
-                if let displayText = displayLabel.text {
-                    //если в надписи всего один символ
-                    if displayText.count == 1 {
-                        //убираем флаг новой строки
-                        isFinishedTypingNumber = false
-                        //если нажатая кнопка - это не точка
-                        if numValue != "." {
-                            //если в надписи единственный символ не 0
-                            if displayText != "0" {
-                                //до добавляем новую цифру
-                                displayLabel.text = displayText + numValue
-                            } else {
-                                //а если в надписи только ноль, то заменяем его новой цифрой
-                                displayLabel.text = numValue
-                            }
-                        } else {
-                            //а если нажата точка, то добавляем "."
-                            displayLabel.text = displayText + "."
-                        }
-                    } else {
-                        //а если в надписи не один символ
-                        //если нажатая кнопка - это не точка
-                        if numValue != "." {
-                            //то просто добавляем эту цифру в надпись
-                            displayLabel.text = displayText + numValue
-                        } else {
-                            //а если нажата точка, то
-                            //если в тексте еще нет точки
-                            if !displayText.contains(".") {
-                                //то добавляем ее в конец строки
-                                displayLabel.text = displayText + "."
-                            }
-                        }
-                    }   //if displayText.count == 1 {
-                }   //if let displayText = displayLabel.text {
-            }   //if let numValue = sender.currentTitle {
-    }   // @IBAction func numButtonPressed(_ sender: UIButton) {
+        //извлекаем текст из нажатой кнопки
+        if let numValue = sender.currentTitle {
+            //если мы начинаем набирать новое число
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                //убираем флаг новой строки
+                isFinishedTypingNumber = false
+            } else {
+                
+                //следующая конструкция необходима для предотвращения повтоного ввода разделителя дробной части числа "."
+                if numValue == "." {
+                    //безопасно преобразовываем опциональную строку в Double
+                    guard let currentDisplayValue = Double(displayLabel.text!) else {
+                        fatalError("Cannot convert display label text to a Double!")
+                    }
+                    //если написанное число равно ему же, но округленному в меньшую сторону
+                    //иначе говоря если написанное число не содержит точки ".",
+                    //иначе говоря если написанное число является целым числом,
+                    //то устанавливаем isInt (это наш флаг целочисленности написанного числа) в true
+                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    
+                    //если написанное число не Int, то больше ничего не делаем
+                    if !isInt {
+                        return
+                    }
+                }
+                //если компилятор дошел до этой строки, значит в написанном точки нет, значит дописываем точку
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
+    }
 
 }
 

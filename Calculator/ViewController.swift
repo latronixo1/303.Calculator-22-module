0010b2869f01cc2a88aca23cd74e9dca3ff3a1fb
latronixo <pamerra@mail.ru>
2025-03-27 10:45:03 +0300

    303. 09:18 Выполнил задание - реализовать нажатие цифр

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -23,8 +23,46 @@ class ViewController: UIViewController {
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a number is entered into the keypad
-    
+        //Что произойдет когда будет нажата цифра или точка на клавиатуре
+        //извлекаем текст из нажатой кнопки
+        if let numValue = sender.currentTitle {
+            //извлекаем текст из надписи
+            if let displayText = displayLabel.text {
+                //если в надписи всего один символ
+                if displayText.count == 1 {
+                    //если нажатая кнопка - это не точка
+                    if numValue != "." {
+                        //если в надписи единственный символ не 0
+                        if displayText != "0" {
+                            //до добавляем новую цифру
+                            displayLabel.text = displayText + numValue
+                        } else {
+                            //а если в надписи только ноль, то заменяем его новой цифрой
+                            displayLabel.text = numValue
+                        }
+                    } else {
+                        //а если нажата точка, то добавляем "."
+                        displayLabel.text = displayText + "."
+                    }
+                } else {
+                    //а если в надписи не один символ
+                    //если нажатая кнопка - это не точка
+                    if numValue != "." {
+                        //то просто добавляем эту цифру в надпись
+                        displayLabel.text = displayText + numValue
+                    } else {
+                        //а если нажата точка, то
+                        //если в тексте еще нет точки
+                        if !displayText.contains(".") {
+                            //то добавляем ее в конец строки
+                            displayLabel.text = displayText + "."
+                        }
+                    }
+
+                }
+            }
+            
+        }
     }
 
 }

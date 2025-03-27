5ccebca08a50802d257e687500dd7378887f1774
latronixo <pamerra@mail.ru>
2025-03-27 11:41:43 +0300

    304.10:56 Реализовал переменную isFinishedTyping

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,58 +12,66 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    //флаг законченности набора цифр (необходимо набирать новую строку). Будет устанавливаться в true при нажатии на операцию (сложения, умножения) для начала набора новой цифры
+    private var isFinishedTypingNumber: Bool = true
     
-    
+    //What should happen when a non-number button is pressed
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+        
     }
 
-    
+    //Что произойдет когда будет нажата цифра или точка на клавиатуре
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        //Что произойдет когда будет нажата цифра или точка на клавиатуре
-        //извлекаем текст из нажатой кнопки
-        if let numValue = sender.currentTitle {
-            //извлекаем текст из надписи
-            if let displayText = displayLabel.text {
-                //если в надписи всего один символ
-                if displayText.count == 1 {
-                    //если нажатая кнопка - это не точка
-                    if numValue != "." {
-                        //если в надписи единственный символ не 0
-                        if displayText != "0" {
-                            //до добавляем новую цифру
-                            displayLabel.text = displayText + numValue
+        //если нам нужно начать новую строку
+//        if isFinishedTypingNumber {
+//            
+//        }   //if isFinishedTypingNumber {
+
+            //извлекаем текст из нажатой кнопки
+            if let numValue = sender.currentTitle {
+                //извлекаем текст из надписи
+                if let displayText = displayLabel.text {
+                    //если в надписи всего один символ
+                    if displayText.count == 1 {
+                        //убираем флаг новой строки
+                        isFinishedTypingNumber = false
+                        //если нажатая кнопка - это не точка
+                        if numValue != "." {
+                            //если в надписи единственный символ не 0
+                            if displayText != "0" {
+                                //до добавляем новую цифру
+                                displayLabel.text = displayText + numValue
+                            } else {
+                                //а если в надписи только ноль, то заменяем его новой цифрой
+                                displayLabel.text = numValue
+                            }
                         } else {
-                            //а если в надписи только ноль, то заменяем его новой цифрой
-                            displayLabel.text = numValue
-                        }
-                    } else {
-                        //а если нажата точка, то добавляем "."
-                        displayLabel.text = displayText + "."
-                    }
-                } else {
-                    //а если в надписи не один символ
-                    //если нажатая кнопка - это не точка
-                    if numValue != "." {
-                        //то просто добавляем эту цифру в надпись
-                        displayLabel.text = displayText + numValue
-                    } else {
-                        //а если нажата точка, то
-                        //если в тексте еще нет точки
-                        if !displayText.contains(".") {
-                            //то добавляем ее в конец строки
+                            //а если нажата точка, то добавляем "."
                             displayLabel.text = displayText + "."
                         }
-                    }
-
-                }
-            }
+                    } else {
+                        //а если в надписи не один символ
+                        //если нажатая кнопка - это не точка
+                        if numValue != "." {
+                            //то просто добавляем эту цифру в надпись
+                            displayLabel.text = displayText + numValue
+                        } else {
+                            //а если нажата точка, то
+                            //если в тексте еще нет точки
+                            if !displayText.contains(".") {
+                                //то добавляем ее в конец строки
+                                displayLabel.text = displayText + "."
+                            }
+                        }
+                        
+                    }   //if displayText.count == 1 {
+                }   //if let displayText = displayLabel.text {
             
-        }
-    }
+            }   //if let numValue = sender.currentTitle {
+    }   // @IBAction func numButtonPressed(_ sender: UIButton) {
 
 }
 
